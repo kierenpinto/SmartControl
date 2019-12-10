2ac6f8056fb5cb2c7ebb85d4102d60bdad653cdc
Kieren Pinto <kieren.pinto@hotmail.com>
2019-12-11 02:05:59 +1100

    firestore triggers iot device

---
+++ web_frontend/functions/firestore_triggers.js
@@ -0,0 +1,27 @@
+// Needed to update the application/devices when changes to database occur.
+
+const functions = require('firebase-functions');
+const {updateLight, updateCurtain} = require('./update_IOT_device');
+
+exports.device_update = functions.firestore.document('devices/{deviceId}').onUpdate((change,context)=>{
+    const deviceObj = change.after.data();
+    //Check object type:
+    const device_type = deviceObj.type;
+    const device_id = change.after.id;
+    let operation_promise = true;
+    console.log(deviceObj,device_id)
+    switch (device_type) {
+        case 'light':
+            console.log("LIGHT chosen")
+            operation_promise = updateLight(device_id,deviceObj.state)
+            break;
+        case 'curtain':
+            console.log("LIGHT chosen")
+            operation_promise = updateCurtain(device_id,deviceObj.state)
+            break;
+        default:
+            console.error("Invalid Device Type")
+            break;
+    }
+    return operation_promise
+})
--- web_frontend/functions/index.js
+++ web_frontend/functions/index.js
@@ -1,13 +1,14 @@
 const admin = require("firebase-admin");
 admin.initializeApp();
 const functions = require('firebase-functions');
-const cors = require('cors')
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
 
+// Google Assistant Actions Endpoint
 const app_fulfillment = require('./fulfillment');
-
 exports.fulfillment = functions.https.onRequest(app_fulfillment);
 
+// Authentication endpoint for Okta and Firebase AUTH for React Client
 const react_client = require('./react_client');
 exports.react_client = functions.https.onRequest(react_client)
+
+const {device_update} = require('./firestore_triggers');
+exports.device_update = device_update
