f08ec078c459f4af4a245582a3a134a09f409232
Kieren Pinto <kieren.pinto@hotmail.com>
2020-06-14 23:10:37 +1000

    removed comments

--- web_frontend/functions/react_client.js
+++ web_frontend/functions/react_client.js
@@ -12,39 +12,10 @@ const oktaJwtVerifier = new OktaJwtVerifier({
     aud: 'api://default'
   }
 });
-/* function authenticationRequired(req, res, next) {
-  const authHeader = req.headers.authorization || '';
-  const match = authHeader.match(/Bearer (.+)/);
-
-  if (!match) {
-    res.status(401);
-    return next('Unauthorized');
-  }
-
-  const accessToken = match[1];
-
-  return oktaJwtVerifier.verifyAccessToken(accessToken,'api://default')
-    .then((jwt) => {
-      req.jwt = jwt;
-      return next();
-    })
-    .catch((err) => {
-      res.status(401).send(err);
-    });
-} */
 
 const app = express();
 
 app.use(cors({ origin: true }));
-/* app.use((cors()));
-app.get('/noauth', (req, res) => {
-  return res.json({
-    message: 'Hello!  There\'s not much to see here :) Please grab one of our front-end samples for use with this sample resource server'
-  });
-});
-app.get('/', authenticationRequired, (req, res) => {
-  return res.json(req.jwt.claims);
-}); */
 
 app.get('/', (req, res) =>{
     const authHeader = req.headers.authorization || ''; // Set the authorization header or '' if not present
